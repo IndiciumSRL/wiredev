09fe23f68a42638aa2bf66ebbefcab6858611467
Joao Mesquita <jmesquita@indicium.com.ar>
2015-06-08 21:41:39 -0300

    Wireportal now has TDD.

--- fabwireportal.py
+++ fabwireportal.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import Queue
 from threading import Timer
@@ -8,6 +9,7 @@ from watchdog.events import LoggingEventHandler, FileSystemEventHandler
 from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
 
 import utils
+from config import config
 
 log = logging.getLogger(__name__)
 
@@ -30,6 +32,32 @@ class CodeEvents(FileSystemEventHandler):
             return
         logging.info('Not reloading because its not a Python file that has changed.')
 
+
+def run_tdd():
+    '''
+    Run unit tests for the project.
+    '''
+    logging.info('Running unit tests')
+    with cd(os.path.join( config.get('vm', 'base_dir') ,'wireportalbackend')):
+        with settings(warn_only=True):
+            result = sudo('/usr/local/bin/py.test')
+            return result.failed
+
+def reload(event):
+    '''
+        Reload source code. For reloading, unit tests are mandatory.
+    '''
+    with settings(warn_only=True):
+        local("osascript -e 'display notification \"File %s changed.\" with title \"Reloading....\"'" % event.src_path)
+    logging.info('Reloading the source.')
+    if run_tdd():
+        with settings(warn_only=True):
+            local("osascript -e 'display notification \"WirePortal unit tests FAILED!.\" with title \"Bummer\"'")    
+            return
+    sudo('supervisorctl restart wiremonitor')
+    with settings(warn_only=True):
+        local("osascript -e 'display notification \"WirePortal reloaded.\" with title \"Matrix Reloaded\"'")
+
 @task
 def run():
     sudo('supervisorctl restart wiremonitor')
@@ -47,11 +75,8 @@ def run():
             except Queue.Empty:
                 continue
             if action == 'reload':
+                reload(event)
                 other_handler.reloading = False
-                utils.growl("File %s changed." % event, "Reloading....")
-                logging.info('Reloading the source.')
-                sudo('supervisorctl restart wiremonitor')
-                utils.growl("WireMonitor reloaded.", "Matrix Reloaded")
 
 
     except KeyboardInterrupt:
